4b1f97e10fb230f309d38a339550292a7420ba53
Nicolás Ferrand <nico_ferrand@hotmail.com>
2024-11-06 11:59:48 -0300

    Agregados y correcciones

--- src/public/index.js
+++ src/public/index.js
@@ -1,11 +1,54 @@
 const socketClient = io();
 
-const products = document.getElementById('products');
+const form = document.getElementById('form');
+const titleIn = document.getElementById('title');
+const descriptionIn = document.getElementById('description');
+const codeIn = document.getElementById('code');
+const priceIn = document.getElementById('price');
+const statusIn = document.getElementById('status');
+const stockIn = document.getElementById('stock');
+const categoryIn = document.getElementById('category');
+const thumbnailsIn = document.getElementById('thumbnails');
+const productsContainer = document.getElementById('products');
 
-socketClient.on("productos", (array)=>{
-    let infoProducts = ''
-    array.map((p)=>{
-        infoProducts += `${p.title} - ${p.decription} - ${p.code} - $${p.price} - ${p.status}- ${p.stock} - ${p.category} - ${p.thumbnails} <br>`
-    })
-    products.innerHTML = infoProducts;
-})
+socketClient.on("productos", (data) => {
+    const products = Array.isArray(data) ? data : data.products;
+
+    let infoProducts = '';
+
+    products.forEach((product) => {
+        infoProducts += `
+        <li>
+            <strong>${product.title}</strong><br>
+            Description: ${product.description}<br>
+            Code: ${product.code}<br>
+            Price: $${product.price}<br>
+            Status: ${product.status === 1 ? 'Disponible' : 'Sin stock'}<br>
+            Stock: ${product.stock}<br>
+            Category: ${product.category}<br>
+            Thumbnails: ${product.thumbnails.length > 0 ? product.thumbnails.join(', ') : 'Sin imagen'}<br>
+            <hr>
+        </li>`;
+    });
+    console.log(infoProducts)
+    
+    productsContainer.innerHTML = infoProducts;
+});
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+
+    const thumbnails = [ thumbnailsIn.value ].filter(e => e.trim() !== '');
+
+    const nuevoProducto = {
+        title: titleIn.value,
+        description: descriptionIn.value,
+        code: codeIn.value,
+        price: priceIn.value,
+        status: statusIn.value,
+        stock: stockIn.value,
+        category: categoryIn.value,
+        thumbnails: thumbnails 
+    };
+    socketClient.emit('agregarProducto', nuevoProducto);
+}
--- src/server.js
+++ src/server.js
@@ -3,13 +3,17 @@ import productRouter from './routes/product_router.js'
 import cartRouter from './routes/cart_router.js'
 import handlebars from 'express-handlebars';
 import path from 'path';
-import { products } from './data/productos.json';
+import products  from './data/productos.json' assert { type: 'json' };
+import ProductManager from './managers/product_manager.js';
 import { Server } from 'socket.io';
+
+const productManager = new ProductManager(`${process.cwd()}/src/data/productos.json`);
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));       
+app.use(express.urlencoded({extended: true})); 
+app.use('/', express.static(path.join(process.cwd(), 'src', 'public')));         
 
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
@@ -17,6 +21,10 @@ app.use('/api/carts', cartRouter);
 app.engine('handlebars', handlebars.engine());
 app.set('views', path.join(process.cwd(), 'src', 'views'));
 app.set('view engine', 'handlebars');
+
+app.get('/', (req, res)=>{
+    res.render('home', {products})
+});
 
 app.get('/realTimeProducts', (req, res)=>{
     res.render('realTimeProducts')
@@ -26,13 +34,16 @@ const httpServer = app.listen(8080, ()=>console.log("Servidor conectado - Puerto
 
 const socketServer = new Server(httpServer);
 
-//app.listen(8080, ()=>console.log("Servidor conectado - Puerto 8080"));
-
 socketServer.on('connection', (socket)=>{
     console.log(`Usuario conectado: ${socket.id}`);
     socket.on('disconnect', ()=>{
         console.log("Usuario desconectado")
     })
 
-    socketServer.emit("productos", {products});  
+    socket.emit("productos", products);  
+
+    socket.on("agregarProducto", (prod) =>{      
+        productManager.create(prod);
+        socket.emit("productos", products)
+    })
 });
